4b2ef2b28e64facce5f22b02848ea971e6430532
hcpzhe <zheyun999@gmail.com>
2014-07-16 17:17:34 +0800

    home/article/read ;设计更新

--- aixin/Lib/Action/Home/ArticleAction.class.php
+++ aixin/Lib/Action/Home/ArticleAction.class.php
@@ -17,10 +17,20 @@ class ArticleAction extends HomebaseAction {
 	 */
 	public function read() {
 		$id = (int)I('get.id');
-		if ($id <= 0) $this->error('参数非法');
-		
+		if ($id <= 0) {
+			$cat = I('get.category');
+			//如果传入category参数
+			//取指定分类的第一条数据
+			if (empty($cat)) $this->error('参数非法');
+			$map['category'] = $cat;
+		} else {
+			$map['id'] = $id;
+		}
 		$map['status'] = '1';
 		
+		$model = New Model('Article');
+		$info = $model->where($map)->find();
+		$this->assign('info',$info);
         cookie('_currentUrl_',$_SERVER['REQUEST_URI']);
 	}
 }
