842196569c163e29a3f6f7850bce436af93d313b
hcpzhe <zheyun999@163.com>
2014-07-18 23:15:52 +0800

    admin/config complete

--- aixin/Lib/Action/Admin/ConfigAction.class.php
+++ aixin/Lib/Action/Admin/ConfigAction.class.php
@@ -19,7 +19,8 @@ class ConfigAction extends AdminbaseAction {
 	 * 需用用ajax来组合数据进行更新 
 	 */
 	public function update() {
-		$datas = I('newdata');
+		$datas = I('post.');
+		$datas = $datas['newdata'];
 		$model = new ConfigModel();
 		$model->startTrans();
 		foreach ($datas as $row) {
