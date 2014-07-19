1a9755a87c3111b32c14f6cf3ef99e17966004ea
hcpzhe <zheyun999@gmail.com>
2014-07-19 09:46:01 +0800

    kindeditor configs fixed

--- index.php
+++ index.php
@@ -11,27 +11,23 @@ define('APP_PATH', './aixin/');
 
 //定义公共目录路径
 define('PUBLIC_PATH', './Public/');
+if (!is_dir(PUBLIC_PATH)) mkdir(PUBLIC_PATH);
 
 //定义公共目录路径
 define('SESSION_PATH', './Session/');
-
-
-//session存储路径生成
 if (!is_dir(SESSION_PATH)) mkdir(SESSION_PATH);
 
+//session存储路径生成
 $path = SESSION_PATH.'Admin/';
 if (!is_dir($path)) mkdir($path);
 
 $path = SESSION_PATH.'Home/';
 if (!is_dir($path)) mkdir($path);
 
-if (!is_dir(PUBLIC_PATH)) mkdir(PUBLIC_PATH);
+//KindEditor编辑器内容 所用图片上传路径
+$path = PUBLIC_PATH.'Kindattached/';
+if (!is_dir($path)) mkdir($path);
 
-//
-////编辑器内容 所用图片上传路径
-//$path = APP_PATH.'Public/textUpload/';
-//if (!is_dir($path)) mkdir($path);
-//
 ////项目_upload文件上传路径
 //$path = APP_PATH.'Public/Uploads/';
 //if (!is_dir($path)) mkdir($path);
